9163fb04a744997f4b1e9e57b69d9f844ba9240e
bilelmoussa <streetvayne@outlook.fr>
2019-05-19 18:57:13 +0100

    Updates

---
+++ precache-manifest.b53f5fd4888334d39c31d878858faa29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c9c54027fd2890e120083ed62861cd",
+    "url": "/elite4you/index.html"
+  },
+  {
+    "revision": "21be95a9f1aecac9de11",
+    "url": "/elite4you/static/css/main.0f22cb03.chunk.css"
+  },
+  {
+    "revision": "68ed08556b5a4ff7352e",
+    "url": "/elite4you/static/js/2.d2d347c2.chunk.js"
+  },
+  {
+    "revision": "21be95a9f1aecac9de11",
+    "url": "/elite4you/static/js/main.61d403f3.chunk.js"
+  },
+  {
+    "revision": "944be8ed5484c5eb4c4e",
+    "url": "/elite4you/static/js/runtime~main.9b0a14d8.js"
+  }
+]);
